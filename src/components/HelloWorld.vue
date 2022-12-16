e5e9a872003cf98c5f0e4727f9c5ac94a763f763
RelaxDegree <1043511190@qq.com>
2022-12-16 22:26:39 +0800

    前两个任务已完成

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -20,13 +20,13 @@
         <div style="width: 60%;margin-left: 20%" class="app-container">
  
           <template>
-            <el-form ref="form" :model="form" label-width="80px">
+            <el-form ref="form" :model="form" label-width="80px" :inline="true">
               <el-form-item label="导入文件">
                 <el-upload
                   class="upload-demo"
                   drag
                   :on-success="readCsv"
-                  action=""
+                  action="https://jsonplaceholder.typicode.com/posts/"
                   :multiple="false"
                   >
                   <i class="el-icon-upload"></i>
@@ -39,7 +39,18 @@
                   <el-radio label="数据库类型"></el-radio>
                 </el-radio-group>
               </el-form-item>
-              
+              <el-form-item label="算法选择">
+                <el-radio-group v-model="form.algorithm">
+                  <el-radio label="Apriori"></el-radio>
+                  <el-radio label="FP-growth"></el-radio>
+                </el-radio-group>
+              </el-form-item>
+              <el-form-item label="支持度">
+                <el-input v-model="form.support" placeholder="请输入支持度"></el-input>
+              </el-form-item>
+              <el-form-item label="置信度">
+                <el-input v-model="form.confident" placeholder="请输入置信度"></el-input>
+              </el-form-item>
             </el-form>
           </template>
         </div>
@@ -51,94 +62,146 @@
  
           <template>
             <el-table
-    :data="tableData"
-    style="width: 100%"
-    :default-sort="{prop: 'date', order: 'descending'}"
-    >
-    <el-table-column
-      prop="date"
-      label="日期"
-      sortable
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="name"
-      label="姓名"
-      sortable
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="address"
-      label="地址"
-      :formatter="formatter">
-    </el-table-column>
-  </el-table>
+              :data="tableData"
+              style="width: 100%"
+              :default-sort="{prop: 'date', order: 'descending'}"
+              >
+              <el-table-column
+                prop="date"
+                label="日期"
+                sortable
+                width="180">
+              </el-table-column>
+              <el-table-column
+                prop="name"
+                label="姓名"
+                sortable
+                width="180">
+              </el-table-column>
+              <el-table-column
+                prop="address"
+                label="地址"
+                :formatter="formatter">
+              </el-table-column>
+            </el-table>
           </template>
         </div>
       <el-button style="margin-top: 12px;margin-left: 20%" type="primary"  @click="next">下一步</el-button>
       </div>
+      <div v-if="active==1 && form.fileType == '文本类型'">
+        <template>
+          <el-table
+            :data="tableData"
+            stripe
+            style="width: 100%">
+              <el-table-column
+              type="index"
+              width="50">
+            </el-table-column>
+            <el-table-column
+              prop="thing"
+              label="事务"
+              width="800">
+            </el-table-column>
+
+          </el-table>
+        </template>
+        <el-button style="margin-top: 12px;margin-left: 20%" type="primary"  @click="next">下一步</el-button>
+      </div>
     </el-main>
     <el-aside width="200px"></el-aside>
   </el-container>
+  
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  },
+
   data() {
       return {
         fileTemp: null,
         active : 0,
         form: {
-          name: '',
-          region: '',
+          fileName: '',
+          algorithm: '',
           fileType: '',
+          support : '',
+          confident : '',
           delivery: false,
           type: [],
           resource: '',
           desc: ''
         },
-        tableData: [{
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }]
+        fileData : [{
+
+        }],
+        tableData: []
       };
     },
     methods: {
       //读取数据
-      readCsv(file) {
-        console.log(file)
+      readCsv(responce, file) {
+        console.log(file.name)
+        this.form.fileName = file.name
       },
-
-
       onSubmit() {
         console.log('submit!');
       },
       next() {
         if (this.active == 0)
         {
-          if (this.form.fileType == '')
+          // console.log(this.form.algorithm)
+          if (this.form.fileType == '' || this.form.support == '' || this.form.confident == '' ||
+              this.algorithm == '')
           {
-              this.open3("请选择上传的文件类型")
+              this.open3("没有填全")
               return;
           }
+          let that = this
+          this.$axios.get('http://127.0.0.1:8000/process/', {
+          params: {
+            fileName : this.form.fileName,
+            support : this.form.support, 
+            confident : this.form.confident,
+            alg : this.form.algorithm,
+          }
+        })
+        .then(function (response) {
+          var table = response.data.data.tabledata
+          var len = table['size']
+          for (var i = 0 ; i < len; i ++)
+          {
+            var lst = ""
+            for (var j in table[i])
+            {
+              if (j !== '')
+                lst = lst + " " + table[i][j]
+            }
+            // console.log(lst)
+            // console.log(this)
+            that.tableData.unshift({thing : lst})
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+          
+        }
+        if (this.active == 1)
+        {
+          this.$axios.get('http://127.0.0.1:8000/result/', {
+          params: {
+          }
+        })
+        .then(function (response) {
+          console.log(response.data)
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
         }
         if (this.active++ > 2) this.active = 0;
         // console.log(this.active)
--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 // import Papa from 'papaparse';
 import 'element-ui/lib/theme-chalk/index.css'
-
- 
+import axios from 'axios'
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 // Vue.use(Papa)
